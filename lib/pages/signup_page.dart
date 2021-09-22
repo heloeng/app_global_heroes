d8bf7755594f9159a822ec6bd865b6900cf94a91
bruno-bergamaschi <brunobergamaschi@live.com>
2021-09-22 11:09:17 -0300

    Ajustes efetuados

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -28,163 +28,153 @@ class _SignupPageState extends State<SignupPage> {
   Widget build(BuildContext context) {
     // Cria o widget Form usando  _formKey
     return Scaffold(
-        body: Column(
+      body: Column(children: [
+        Expanded(
+          child: Container(
+            color: Color.fromRGBO(16, 16, 16, 1),
+            padding: EdgeInsets.all(30),
+            child: SingleChildScrollView(
+              child: Column(children: [
+                Form(
+                  key: _formKey,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          // O validador recebe o texto digitado
+                          decoration: InputDecoration(labelText: 'Nome'),
+                          onChanged: (texto) => nome = texto,
+                          validator: (String? texto) {
+                            //string null label testar se
+                            //se o texto é diferente de nulo e não está vazio
+                            if (texto != null && texto.isNotEmpty) {
+                              print(texto.length);
+                              if (texto.length < 3) {
+                                return "Digite um Nome válido.";
+                              }
+                            } else {
+                              return "Campo obrigatório.";
+                            }
+                          },
+                        ),
+                        TextFormField(
+                          validator: (String? texto) {
+                            if (texto != null && texto.isNotEmpty) {
+                              if (!texto.contains('@') || texto.length < 8) {
+                                return 'Digite um email válido';
+                              }
+                            } else {
+                              return 'Campo obrigatório.';
+                            }
+                          },
+                          onChanged: (texto) => email = texto,
+                          style: TextStyle(color: Colors.white),
+                          keyboardType: TextInputType.emailAddress,
+                          autofocus: true,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(
+                              Icons.mail_outline,
+                            ),
+                            labelText: 'Email',
+                            //border: OutlineInputBorder(),
+                          ),
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: 'Nickname'),
+                          onChanged: (texto) => nickName = texto,
+                          validator: (String? texto) {
+                            //string null label testar se
+                            //se o texto é diferente de nulo e não está vazio
+                            if (texto != null && texto.isNotEmpty) {
+                              print(texto.length);
+                              if (texto.length < 1) {
+                                return "Digite um Nickname válido.";
+                              }
+                            } else {
+                              return "Campo obrigatório.";
+                            }
+                          },
+                        ),
+                        TextFormField(
+                          onChanged: (texto) => senha = texto,
+                          obscureText: true,
+                          autofocus: true,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.vpn_key_sharp),
+                            labelText: 'Senha',
+                            //border:OutlineInputBorder(),
+                          ),
+                          validator: (String? texto) {
+                            if (texto != null && texto.isNotEmpty) {
+                              if (texto.length < 8) {
+                                return "Digite uma senha com 8 caracteres ou mais";
+                              }
+                            } else {
+                              return "Campo obrigatório";
+                            }
+                          },
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.vpn_key_sharp),
+                              labelText: 'Confirmar Senha'),
+                          onChanged: (texto) => confirmarSenha = texto,
+                          obscureText: true,
+                          autofocus: true,
+                          validator: (String? texto) {
+                            if (texto != null && texto.isNotEmpty) {
+                              if (texto.length < 8) {
+                                return "Digite uma senha com 8 caracteres ou mais";
+                              }
+                            } else {
+                              return "Campo obrigatório";
+                            }
+                          },
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(20),
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              if (_formKey.currentState?.validate() ?? false) {
+                                final List<String> favoritos = [];
+                                final user = UserModel(
+                                    nome: nome,
+                                    email: email,
+                                    nickName: nickName,
+                                    favoritos: favoritos);
+                                await userController.signup(
+                                    email, senha, user, favoritos);
 
-          children: [
-            Expanded(
-              child: Container(
-                color: Color.fromRGBO(16, 16, 16, 1),
-                padding: EdgeInsets.all(30),
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      Form(
-                        key: _formKey,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            children: [
-                              TextFormField(
-                                // O validador recebe o texto digitado
-                                decoration: InputDecoration(labelText: 'Nome'),
-                                onChanged: (texto) => nome = texto,
-                                validator: (String? texto) {
-                                  //string null label testar se
-                                  //se o texto é diferente de nulo e não está vazio
-                                  if (texto != null && texto.isNotEmpty) {
-                                    print(texto.length);
-                                    if (texto.length < 3) {
-                                      return "Digite um Nome válido.";
-                                    }
-                                  } else {
-                                    return "Campo obrigatório.";
-                                  }
-                                },
-                              ),
-                                TextFormField(
-                  validator: (String? texto) {
-                    if (texto != null && texto.isNotEmpty) {
-                      if (!texto.contains('@') || texto.length < 8) {
-                        return 'Digite um email válido';
-                      }
-                    } else {
-                      return 'Campo obrigatório.';
-                    }
-                  },
-                  onChanged: (texto) => email = texto,
-                  style: TextStyle(color: Colors.white),
-                  keyboardType: TextInputType.emailAddress,
-                  autofocus: true,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(
-                      Icons.mail_outline,
+                                Navigator.pop(context);
+                              }
+                            },
+                            child: Text("Criar conta"),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 15),
+                          child: OutlinedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => LoginPage(),
+                                ),
+                              );
+                            },
+                            child: Text("Tenho Cadastro"),
+                          ),
+                        ),
+                      ],
                     ),
-                    labelText: 'Email',
-                    //border: OutlineInputBorder(),
                   ),
                 ),
-                              TextFormField(
-                                decoration:
-                                    InputDecoration(labelText: 'Nickname'),
-                                onChanged: (texto) => nickName = texto,
-                                validator: (String? texto) {
-                                  //string null label testar se
-                                  //se o texto é diferente de nulo e não está vazio
-                                  if (texto != null && texto.isNotEmpty) {
-                                    print(texto.length);
-                                    if (texto.length < 1) {
-                                      return "Digite um Nickname válido.";
-                                    }
-                                  } else {
-                                    return "Campo obrigatório.";
-                                  }
-                                },
-                              ),
-                              TextFormField(
-                                onChanged: (texto) => senha = texto,
-                                  obscureText: true,
-                                  autofocus: true,
-                                  decoration: InputDecoration(
-                                    prefixIcon: Icon(Icons.vpn_key_sharp),
-                                    labelText: 'Senha',
-                                    //border:OutlineInputBorder(),
-                                  ),
-                                  validator: (String? texto) {
-                                    if (texto != null && texto.isNotEmpty) {
-                                      if (texto.length < 8) {
-                                        return "Digite uma senha com 8 caracteres ou mais";
-                                      }
-                                    } else {
-                                      return "Campo obrigatório";
-                                    }
-                                  },
-                                  
-                                  ),
-                              TextFormField(
-                                decoration: InputDecoration(
-                                    prefixIcon: Icon(Icons.vpn_key_sharp),
-                                    labelText: 'Confirmar Senha'),
-                                onChanged: (texto) => confirmarSenha = texto,
-                                obscureText: true,
-                                autofocus: true,
-                                validator: (String? texto) {
-                                  if (texto != null && texto.isNotEmpty) {
-                                    if (texto.length < 8) {
-                                      return "Digite uma senha com 8 caracteres ou mais";
-                                    }
-                                  } else {
-                                    return "Campo obrigatório";
-                                  }
-                                },
-                              ),
-                              Container(
-                                padding: EdgeInsets.all(20),
-                                child: ElevatedButton(
-                                  onPressed: () async {
-                                    if (_formKey.currentState?.validate() ??
-                                        false) {
-                                      final List<String> favoritos = [];
-                                      final user = UserModel(
-                                          nome: nome,
-                                          email: email,
-                                          nickName: nickName,
-                                          favoritos: favoritos);
-                                      await userController.signup(
-                                          email, senha, user, favoritos);
-
-                                      Navigator.pop(context);
-                                    }
-                                  },
-                                  child: Text("Criar conta"),
-                                ),
-                              ),
-                              Container(
-                                margin: EdgeInsets.only(top: 15),
-                                child: OutlinedButton(
-                                  onPressed: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => LoginPage(),
-                                      ),
-                                    );
-                                  },
-                                  child: Text("Tenho Cadastro"),
-                                ),
-                              ),
-                            ],
-
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              ]),
             ),
           ),
-        )
-      ],
-    ));
+        ),
+      ]),
+    );
   }
 }
