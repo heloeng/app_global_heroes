c9753e3615ad844957847c666b6538d25e5e8e0d
bruno-bergamaschi <brunobergamaschi@live.com>
2021-09-29 13:37:08 -0300

    correção na msg de erro no login e cadastro

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -99,7 +99,8 @@ class _LoginPageState extends State<LoginPage> {
                                     msg =
                                         "Login com email e senha não está habilitado";
                                   } else {
-                                    msg = "Ocorreu um erro";
+                                    msg =
+                                        "Informe seu email e senha de cadastro";
                                   }
 
                                   ScaffoldMessenger.of(context).showSnackBar(
